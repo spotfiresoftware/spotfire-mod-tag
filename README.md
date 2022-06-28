5a0d9d748104cb21afbc8fb54fe7b7522ae97778
Arnaud Varin <avarin@tibco.com>
2022-06-28 17:19:22 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,78 @@
+# Tag Visualization Mod for TIBCO Spotfire®
+
+The Tag Visualization Mod displays a set of selectable tags that can be used to query a dataset based on keywords.
+
+<img width="1366" alt="Tags Screenshot" src="screenshots/Continents.png">
+
+Each tag represents a unique comma separated value in a column. Selecting a tag will mark all rows where the corresponding value occurs. This marking can then be used to limit the data displayed in other visualizations.
+
+## Try this mod in TIBCO Spotfire® Analyst
+
+### How to open the mod
+
+1. Open Spotfire® Analyst, and create and analysis by loading some data.
+2. Unzip the downloaded file, and locate the .mod file in the unzipped folder.
+3. Drag the file into the analysis.
+4. The visualization mod is added to the analysis.
+5. To learn more about the capabilities and limitations of this visualization mod, keep reading or see the User Guide which is also located in the downloaded folder.
+6. For general information on how to use and share visualization mods, you can read the Spotfire documentation.
+
+## Data requirement
+
+The tag visualization mod can be used to display tags in any data set with at least one categorical column.
+
+Note that every mod handles missing, corrupted and/or inconsistent data in different ways. It is advised to always review how the data is visualized.
+
+## Setting up the Tag Visualization
+
+Let's say we have data about the people and their pets:
+
+| Name   | Pet             |
+| ------ | --------------- |
+| Frank  | Dog             |
+| Joe    | Dog, Cat        |
+| Oliver | Cat,Dog         |
+| Anna   | Dog, Cat        |
+| Brie   | Dog, Cat, Mouse |
+| Paula  | Cat, Mouse      |
+| Eric   | Cat             |
+| Jean   | Cat             |
+
+A basic tag visualization can be configured to show one tag per Pet with the following settings:
+
+-   Tags = Pet
+
+The resulting tag list will look like this:
+
+<img alt="Tags Screenshot" src="screenshots/Pets.png">
+
+## Configuring the Tag Visualization
+
+The Tag Visualization can be configured by setting the tag axis.
+
+## Using the Tag Visualization
+
+### Marking
+
+Clicking on a tag will mark all rows with the corresponding key value in all other visualizations that uses the same marking. You can mark several tags by Ctrl-clicking on them.
+
+Tags are marked when all rows corresponding to that tag is marked.
+
+## Version History
+
+### 1.0
+
+-   First version
+
+## Developing the Tag Visualization
+
+Build Project
+
+In a terminal window:
+
+-   `npm install`
+-   `npm run build-watch`
+
+In a new terminal window
+
+-   `npm run server`
---
+++ src/index.ts
@@ -0,0 +1,157 @@
+import { DataView, DataViewRow } from "spotfire-api";
+import * as d3 from "d3";
+
+interface Tag {
+    name: string;
+    rows: DataViewRow[];
+    isMarked: boolean;
+}
+
+const Spotfire = window.Spotfire;
+
+const DEBUG = false;
+
+const tagAxisName = "Tags";
+
+const modContainer = d3.select("#mod-container");
+
+Spotfire.initialize(async (mod) => {
+    /**
+     * Initialize render context - should show 'busy' cursor.
+     * A necessary step for printing (another step is calling render complete)
+     */
+    const context = mod.getRenderContext();
+
+    const reader = mod.createReader(mod.visualization.data(), mod.windowSize());
+
+    reader.subscribe(generalErrorHandler(mod)(onChange), (err) => {
+        mod.controls.errorOverlay.show(err);
+    });
+
+    async function onChange(dataView: DataView, windowSize: Spotfire.Size) {
+        /**
+         * Set font-style from canvas
+         */
+        document.querySelector("#extra_styling")!.innerHTML = `
+        .body { 
+            font-size: ${context.styling.general.font.fontSize}px; 
+            font-weight: ${context.styling.general.font.fontWeight}; 
+            font-style: ${context.styling.general.font.fontStyle};
+        }
+        `;
+
+        /**
+         * Validate axis configuration
+         */
+        const hasTags = !!(await dataView.categoricalAxis(tagAxisName));
+        if (!hasTags) {
+            modContainer.selectAll("*").remove();
+            return;
+        }
+
+        /**
+         * Extract tags from comma separated keywords in the selected column
+         */
+        let rows = (await dataView.allRows()) || [];
+
+        let tags = new Map();
+
+        rows.forEach((row: DataViewRow) => {
+            let tagValue = row.categorical(tagAxisName).formattedValue();
+            let tagNames = tagValue.split(",").map((name) => name.trim());
+            tagNames.forEach((tagName) => {
+                if (!tags.has(tagName)) {
+                    let tag: Tag = {
+                        name: tagName,
+                        rows: [row],
+                        isMarked: row.isMarked()
+                    };
+                    tags.set(tagName, tag);
+                } else {
+                    let tag = tags.get(tagName);
+                    tag.rows.push(row);
+                    if (!row.isMarked()) {
+                        tag.isMarked = false; // tag should only be marked if all corresponding rows are marked
+                    }
+                }
+            });
+        });
+
+        let tagArray: Tag[] = Array.from(tags.values()).sort();
+
+        /**
+         * Update the tag display
+         */
+        modContainer
+            .selectAll("div")
+            .data(tagArray)
+            .join("div")
+            .attr("class", "tag")
+            .text((tag: Tag) => tag.name)
+            .classed("markedTag", (tag: Tag) => tag.isMarked)
+            .on("click", (event, tag: Tag) =>
+                tag.rows.forEach((row: DataViewRow) =>
+                    row.mark(event.ctrlKey || event.metaKey ? "ToggleOrAdd" : "Replace")
+                )
+            );
+
+        context.signalRenderComplete();
+    }
+});
+
+/**
+ * subscribe callback wrapper with general error handling, row count check and an early return when the data has become invalid while fetching it.
+ *
+ * The only requirement is that the dataview is the first argument.
+ * @param mod - The mod API, used to show error messages.
+ * @param rowLimit - Optional row limit.
+ */
+export function generalErrorHandler<T extends (dataView: Spotfire.DataView, ...args: any) => any>(
+    mod: Spotfire.Mod,
+    rowLimit = 2000
+): (a: T) => T {
+    return function (callback: T) {
+        return async function callbackWrapper(dataView: Spotfire.DataView, ...args: any) {
+            try {
+                const errors = await dataView.getErrors();
+                if (errors.length > 0) {
+                    mod.controls.errorOverlay.show(errors, "DataView");
+                    return;
+                }
+                mod.controls.errorOverlay.hide("DataView");
+
+                /**
+                 * Hard abort if row count exceeds an arbitrary selected limit
+                 */
+                const rowCount = await dataView.rowCount();
+                if (rowCount && rowCount > rowLimit) {
+                    mod.controls.errorOverlay.show(
+                        `☹️ Cannot render - too many rows (rowCount: ${rowCount}, limit: ${rowLimit}) `,
+                        "General"
+                    );
+                    return;
+                }
+
+                /**
+                 * User interaction while rows were fetched. Return early and respond to next subscribe callback.
+                 */
+                const allRows = await dataView.allRows();
+                if (allRows == null) {
+                    return;
+                }
+
+                await callback(dataView, ...args);
+
+                mod.controls.errorOverlay.hide("General");
+            } catch (e) {
+                if (e instanceof Error) {
+                    mod.controls.errorOverlay.show(e.message, "General");
+
+                    if (DEBUG) {
+                        throw e;
+                    }
+                }
+            }
+        } as T;
+    };
+}
